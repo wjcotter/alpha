051a12cfa7ecf094bc74d949ba6cfddf16fb7e5a
Bill Cotter <wjcotter@rocketmail.com>
2013-01-15 15:35:01 -0500

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,111 @@
+<?php
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
+
+if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
+
+    // Local Environment
+    define('WP_ENV', 'local');
+    define('WP_DEBUG', true);
+
+    require( 'wp-config-local.php' );
+
+} elseif ( file_exists( dirname( __FILE__ ) . '/wp-config-playground.php' ) ) {
+
+    // Playground Environment
+    define('WP_ENV', 'playground');
+    define('WP_DEBUG', true);
+
+    require( 'wp-config-playground.php' );
+
+} elseif ( file_exists( dirname( __FILE__ ) . '/wp-config-production.php' ) ) {
+
+    // Production Environment
+    define('WP_ENV', 'production');
+    define('WP_DEBUG', false);
+
+    require( 'wp-config-production.php' );
+}
+
+
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+
+// ===============================
+// Switch SITEURL & HOME Constants
+// ===============================
+
+define( 'RELOCATE', true);
+
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ==============================================================
+// Salts, for security
+// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+// ==============================================================
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+// ==============================================================
+// Table prefix
+// Change this if you have multiple installs in the same database
+// ==============================================================
+$table_prefix  = 'wp_';
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
+
+// ===========
+// Hide errors
+// ===========
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
+
+// =================================================================
+// Debug mode
+// Debugging? Enable these. Can also enable them in local-config.php
+// =================================================================
+// define( 'SAVEQUERIES', true );
+// define( 'WP_DEBUG', true );
+
+// ======================================
+// Load a Memcached config if we have one
+// ======================================
+if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
+	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+
+// ===========================================================================================
+// This can be used to programatically set the stage when deploying (e.g. production, staging)
+// ===========================================================================================
+define( 'WP_STAGE', '%%WP_STAGE%%' );
+define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
+
+// ===================
+// Bootstrap WordPress
+// ===================
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+require_once( ABSPATH . 'wp-settings.php' );
+
+
+//==================================
+//Bypass FTP connection credentials:
+//==================================
+define('FS_METHOD','direct');
